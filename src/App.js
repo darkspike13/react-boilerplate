7cccfd72aa096f6d32ccd3f5fa2c451cebb3e0bb
Guillaume <guillaume.assaud@53north.digital>
2021-05-14 15:52:55 -0400

    INIT: Router

--- src/App.js
+++ src/App.js
@@ -1,23 +1,41 @@
-function App() {
-  return (
-      <>
-          <header className="header">
-              <p>
-                  Header
-              </p>
-          </header>
-          <div className="">
-              <p>
-                  SandBox Running
-              </p>
-          </div>
-          <footer className="footer">
-              <p>
-                  Footer
-              </p>
-          </footer>
-      </>
-  );
-}
+import {
+  BrowserRouter as Router,
+  Link,
+  Redirect,
+  Route,
+  Switch,
+} from 'react-router-dom';
+
+const App = () => (
+    <Router>
+        <header>
+            <nav>
+                <ul>
+                    <li>
+                        <Link to="/">Home</Link>
+                    </li>
+                    <li>
+                        <Link to="/about">About</Link>
+                    </li>
+                </ul>
+            </nav>
+        </header>
+        <Switch>
+            <Route path="/about">
+                <About />
+            </Route>
+            <Route path="*">
+                <Redirect to="/" />
+            </Route>
+            <Route path="/">
+                <Home />
+            </Route>
+        </Switch>
+        <footer />
+    </Router>
+);
+
+const Home = () => <h2>Home</h2>;
+const About = () => <h2>About</h2>;
 
 export default App;

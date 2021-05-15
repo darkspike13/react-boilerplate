f8a107d09fb21422e57c3619b340fb28542beca0
Guillaume <guillaume.assaud@53north.digital>
2021-05-15 18:34:43 -0400

    Edge Rendering

---
+++ src/edge/index.js
@@ -0,0 +1,64 @@
+import React from 'react';
+import ReactDOMServer from 'react-dom/server';
+import App from '../App';
+
+const indexFile = `
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1" />
+    <meta name="theme-color" content="#000000" />
+    <meta
+      name="description"
+      content="Web site created using create-react-app"
+    />
+    <title>React App</title>
+  </head>
+  <body>
+    <noscript>You need to enable JavaScript to run this app.</noscript>
+    <div id="root"></div>
+    <div>Rendered on Edge</div>
+  </body>
+</html>
+`;
+
+const handler = async (event) => {
+  try {
+    const { request } = event.Records[0].cf;
+    console.log({ request });
+    if (request.uri === '/test') {
+      return request;
+    }
+    const app = ReactDOMServer.renderToString(<App />);
+    const html = indexFile.replace(
+      '<div id="root"></div>',
+      `<div id="root">${app}</div>`,
+    );
+    return {
+      status: '200',
+      statusDescription: 'OK',
+      headers: {
+        'cache-control': [
+          {
+            key: 'Cache-Control',
+            value: 'max-age=100',
+          },
+        ],
+        'content-type': [
+          {
+            key: 'Content-Type',
+            value: 'text/html',
+          },
+        ],
+      },
+      body: html,
+    };
+    // return request;
+  } catch (error) {
+    console.log(`Error ${error.message}`);
+    return `Error ${error}`;
+  }
+};
+
+export default { handler };
